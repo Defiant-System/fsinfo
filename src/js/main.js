799b4b17d9b45f815055582ddedacbf5b8f73d16
Hakan Bilgin <hbi99@hotmail.com>
2021-05-07 22:50:39 +0200

    opening fs-info from contextmenu

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,24 @@
 const fsinfo = {
 	init() {
 		// fast references
-		this.content = window.find("content");
+		this.els = {
+			content: window.find("content"),
+		};
 
 		// window height should obey contents height
 		window.height = "auto";
 	},
 	dispatch(event) {
+		let Self = fsinfo,
+			el;
 		switch (event.type) {
+			// system events
 			case "window.open":
+				break;
+			// custom events
+			case "show-item-info":
+				Self.els.content.prop({ className: `kind-${event.parsed.kind}` });
+				console.log(event);
 				break;
 			case "toggle-wrapper":
 				event.el.toggleClass("expanded", event.el.hasClass("expanded"));
