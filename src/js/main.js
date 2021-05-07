1cd17b61ba3c172fa608e65ccdb6ea5c0321ade2
Hakan Bilgin <hbi99@hotmail.com>
2021-05-07 19:27:25 +0200

    ui

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,16 @@ const fsinfo = {
 	init() {
 		// fast references
 		this.content = window.find("content");
+
+		// window height should obey contents height
+		window.height = "auto";
 	},
 	dispatch(event) {
 		switch (event.type) {
 			case "window.open":
 				break;
-			case "open-help":
-				defiant.shell("fs -u '~/help/index.md'");
+			case "toggle-wrapper":
+				event.el.toggleClass("expanded", event.el.hasClass("expanded"));
 				break;
 		}
 	}
